75fd21158a5011b3dbc3c09445721689e42ed2df
Kelvin Santos <me@quleuber.dev>
2022-06-08 15:09:22 -0300

    TODO

--- pages/blocks/[id].tsx
+++ pages/blocks/[id].tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 const Block: NextPage = () => {
   const router = useRouter()
   const { id } = router.query
+  // TODO: Why SSR not working with `id` ?
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       Showing block: {id}
